cd6f52c599a5b652812d36172d41f656e4252862
webmanagementdgn <webdegansts@gmail.com>
2023-06-05 08:56:30 -0700

    making the hero a bit more modern

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <h1>Desclaimer!</h1>
       <p>
         This is a demo web app you will not be able to watch any of the movies
-        shown in the home page in this website
+        shown in the home page of this website
       </p>
     </div>
   );
